7355f3df9b835b41243e7097a455359040bca34e
Henrod <henrique.93.rodrigues@gmail.com>
2016-11-19 18:03:52 -0200

    correct pins for LCD

--- bedroom/tv.js
+++ bedroom/tv.js
@@ -1,10 +1,10 @@
 var Lcd = require('lcd'),
-  lcd = new Lcd({rs: 45, e: 44, data: [66, 67, 68, 69], cols: 16, rows: 2});
+lcd = new Lcd({rs: 18, e: 23, data: [12, 16, 20, 21], cols: 16, rows: 2});
  
 lcd.on('ready', function () {
   setInterval(function () {
     lcd.setCursor(0, 0);
-    lcd.print(new Date().toISOString().substring(11, 19), function (err) {
+    lcd.print("Hello, World!", function (err) {
       if (err) {
         throw err;
       }
